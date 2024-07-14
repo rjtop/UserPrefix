5976a92fd700217898949e3eba68568401da9f57
rjtop <90399695+rjtop@users.noreply.github.com>
2024-07-14 16:17:11 +0800

    Update PrefixManager.java

--- src/main/java/cc/carm/plugin/userprefix/manager/PrefixManager.java
+++ src/main/java/cc/carm/plugin/userprefix/manager/PrefixManager.java
@@ -11,7 +11,6 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
